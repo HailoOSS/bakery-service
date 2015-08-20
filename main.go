1a3a8ba6f219cdb20bd1b7dab45d4ed06dd5f013
Simon Thulbourn <simon+github@thulbourn.com>
2015-08-20 13:41:08 +0100

    something something
    Signed-off-by: Simon Thulbourn <simon+github@thulbourn.com>

--- main.go
+++ main.go
@@ -4,32 +4,27 @@ import (
 	log "github.com/cihub/seelog"
 
 	"github.com/hailocab/bakery-service/handler"
-	fooproto "github.com/hailocab/bakery-service/proto/foo"
 	service "github.com/hailocab/go-platform-layer/server"
 )
 
 func main() {
 	defer log.Flush()
 
-	service.Name = "com.hailocab.service.bakery"
-	service.Description = "Please provide a short description of what your service does. It should be about this long."
+	service.Name = "com.hailocab.infrastructure.bakery"
+	service.Description = "Makes artefacts"
 	service.Version = ServiceVersion
 	service.Source = "github.com/hailocab/bakery-service"
-	service.OwnerEmail = "jonathan@hailocab.com"
-	service.OwnerMobile = "+4407546186424"
-	service.OwnerTeam = "h2o"
+	service.OwnerEmail = "platform@hailocab.com"
+	service.OwnerTeam = "Platform"
 
 	service.Init()
 
 	service.Register(&service.Endpoint{
-		Name:             "foo",
-		Mean:             50,
-		Upper95:          100,
-		Handler:          handler.Foo,
-		RequestProtocol:  new(fooproto.Request),
-		ResponseProtocol: new(fooproto.Response),
-		// remember to choose an appropriate authoriser for each endpoint
-		Authoriser: service.RoleAuthoriser([]string{"ADMIN"}),
+		Name:       "foo",
+		Mean:       50,
+		Upper95:    100,
+		Handler:    handler.Foo,
+		Authoriser: service.RoleAuthoriser([]string{"ADMIN", "PLATFORM"}),
 	})
 
 	service.Run()
