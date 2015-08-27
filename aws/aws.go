5a911d6526dd59a53a0b15a9cff41bb390045140
Simon Thulbourn <simon+github@thulbourn.com>
2015-08-27 09:37:26 +0100

    Add aws pkg, allows service to assume role
    Signed-off-by: Simon Thulbourn <simon+github@thulbourn.com>

--- aws/aws.go
+++ aws/aws.go
@@ -1,7 +1,92 @@
 package aws
 
 import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/hailocab/go-service-layer/config"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	log "github.com/cihub/seelog"
 )
 
-var _ = aws.SDKName
+var (
+	DefaultAccount string = "864806739507"
+	accounts       []Account
+)
+
+func Init() {
+	var err error
+
+	accounts, err = loadAccountInfo()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Auth(accountId string) (*aws.Config, error) {
+	var account Account
+	for _, a := range accounts {
+		if a.Id == accountId {
+			account = a
+			break
+		}
+	}
+
+	return account.AssumeRole(randString(), 3600)
+}
+
+func GetS3Object(bucket string, key string) (io.ReadCloser, error) {
+	config, err := Auth(DefaultAccount)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to auth: %v", err)
+	}
+
+	svc := s3.New(config)
+
+	resp, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("Unable to fetch '%s/%s': %v", bucket, key, err)
+	}
+
+	return resp.Body, nil
+}
+
+func loadAccountInfo() ([]Account, error) {
+	accountConfig := config.AtPath(
+		"hailo",
+		"service",
+		"bakery",
+		"accounts",
+	).AsJson()
+
+	log.Debugf("Bakery accounts: %v", accountConfig)
+
+	var accs []Account
+	if err := json.Unmarshal(accountConfig, &accs); err != nil {
+		return nil, err
+	}
+
+	log.Debugf("Actual accounts: %v", accs)
+	return accs, nil
+}
+
+func randString() string {
+	alphanum := "0123456789abcdefghigklmnopqrst"
+
+	var bytes = make([]byte, 10)
+	rand.Read(bytes)
+
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+
+	return string(bytes)
+}
